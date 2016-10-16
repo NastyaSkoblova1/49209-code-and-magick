80534fb8178400ca64c2fc5074fc550e4346189c
Nastya <nastyaskoblova@mail.ru>
2016-10-16 18:37:42 +0300

    add functions for check of form

--- src/js/form.js
+++ src/js/form.js
@@ -32,3 +32,75 @@ window.form = (function() {
 
   return form;
 })();
+
+var MIN_GOOD_RATE = 3;
+var rate = document.querySelectorAll('.review-form-group-mark input[type="radio"]');
+var rateGroup = document.querySelector('.review-form-group-mark');
+var nameText = document.querySelector('#review-name');
+var reviewText = document.querySelector('#review-text');
+var hintControls = document.querySelector('.review-fields');
+var nameHint = document.querySelector('.review-fields-name');
+var textHint = document.querySelector('.review-fields-text');
+var submitButton = document.querySelector('.review-submit');
+
+var getRateValue = function() {
+  for (var i = 0; i < rate.length; i++) {
+    var rateValue;
+    if (rate[i].checked) {
+      rateValue = rate[i].getAttribute('value');
+    }
+  }
+  return rateValue;
+};
+
+var setRequired = function() {
+  if (getRateValue() < MIN_GOOD_RATE) {
+    reviewText.setAttribute('required', '');
+  }
+};
+
+var checkNameField = function() {
+  if (nameText.value !== '') {
+    nameHint.style.display = 'none';
+  }
+};
+
+var checkTextField = function() {
+  if (reviewText.value !== '') {
+    textHint.style.display = 'none';
+  }
+};
+
+var checkControlsField = function() {
+  if (nameText.value !== '' && reviewText.value !== '') {
+    hintControls.style.display = 'none';
+  }
+};
+
+var changeDisabledButton = function() {
+  if (getRateValue() < MIN_GOOD_RATE) {
+    if (nameText.value !== '' && reviewText.value !== '') {
+      submitButton.removeAttribute('disabled');
+    }
+  } else {
+    if (nameText.value !== '') {
+      submitButton.removeAttribute('disabled');
+    }
+  }
+};
+
+rateGroup.onchange = function() {
+  setRequired();
+};
+
+nameText.onblur = function() {
+  checkNameField();
+  checkControlsField();
+  changeDisabledButton();
+};
+
+reviewText.onblur = function() {
+  checkTextField();
+  checkControlsField();
+  changeDisabledButton();
+};
