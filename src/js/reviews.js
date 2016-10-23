b00704a63aae5ef292f2edb007e9e37d7dc92ec0
Nastya <nastyaskoblova@mail.ru>
2016-10-23 23:59:34 +0300

    fix eslint bugs, add alt, review

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -9,7 +9,7 @@
     'review_usefulness': 10,
     'rating': 2,
     'description': 'Плохая игра: слишком сильно затягивает и невозможно оторваться. Я потерял работу, учебу, девушку и дар речи, но продолжаю играть. Это призыв о помощи: спасите.'
-    }, {
+  }, {
     'author': {
       'name': 'Ксения Собчак',
       'picture': 'img/user-3.png'
@@ -17,7 +17,7 @@
     'review_usefulness': 6,
     'rating': 5,
     'description': 'Все хорошо, мне нравится.'
-    }, {
+  }, {
     'author': {
       'name': 'Ксюша Бородина',
       'picture': 'img/user-2.png'
@@ -25,7 +25,7 @@
     'review_usefulness': 3,
     'rating': 1,
     'description': 'Все плохо, мне не нравится'
-    }, {
+  }, {
     'author': {
       'name': 'Мария Антуанетта',
       'picture': 'img/user-1.jpg'
@@ -33,7 +33,7 @@
     'review_usefulness': 4,
     'rating': 3,
     'description': 'Невероятно чумовая игра. Пендальф-синий — мой герой)))) Он такой милашка. Благодаря ему я наконец нацчилась отвлекаться от работы и учёбы.'
-    }, {
+  }, {
     'author': {
       'name': 'Дмитрий Карпов',
       'picture': 'img/user-3.png'
@@ -41,7 +41,7 @@
     'review_usefulness': 20,
     'rating': 4,
     'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
-    }, {
+  }, {
     'author': {
       'name': 'Максим Шаровары',
       'picture': 'img/user-1.jpg'
@@ -49,15 +49,15 @@
     'review_usefulness': 115,
     'rating': 2,
     'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
-    }, {
+  }, {
     'author': {
       'name': 'Зулейха Валиева',
       'picture': 'img/user-3.png'
     },
-   'review_usefulness': 10,
+    'review_usefulness': 10,
     'rating': 4,
     'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
-    }, {
+  }, {
     'author': {
       'name': 'Федор Непомнящих',
       'picture': 'img/user-2.png'
@@ -65,7 +65,7 @@
     'review_usefulness': 10,
     'rating': 3,
     'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
-    }, {
+  }, {
     'author': {
       'name': 'Макаронный Монстр',
       'picture': 'img/user-1.jpg'
@@ -73,7 +73,7 @@
     'review_usefulness': -3,
     'rating': 5,
     'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
-    }, {
+  }, {
     'author': {
       'name': 'Миклухо Маклай',
       'picture': 'img/user-3.png'
@@ -81,7 +81,7 @@
     'review_usefulness': 0,
     'rating': 2,
     'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
-    }, {
+  }, {
     'author': {
       'name': 'Муравьев Апостол',
       'picture': 'img/user-2.png'
@@ -89,7 +89,7 @@
     'review_usefulness': 0,
     'rating': 1,
     'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
-    }, {
+  }, {
     'author': {
       'name': 'Максим Горький',
       'picture': 'img/user-3.png'
@@ -97,7 +97,7 @@
     'review_usefulness': 8,
     'rating': 3,
     'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
-    }, {
+  }, {
     'author': {
       'name': 'Аноним',
       'picture': 'img/ijwdoq'
@@ -105,7 +105,7 @@
     'review_usefulness': 102,
     'rating': 3,
     'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
-    }, {
+  }, {
     'author': {
       'name': 'Иван Иванов',
       'picture': 'img/user-1.jpg'
@@ -113,7 +113,7 @@
     'review_usefulness': 5,
     'rating': 4,
     'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
-    }, {
+  }, {
     'author': {
       'name': 'Василиса Васильева',
       'picture': 'img/user-2.png'
@@ -121,7 +121,7 @@
     'review_usefulness': 0,
     'rating': 4,
     'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
-    }, {
+  }, {
     'author': {
       'name': 'Хороший Человек',
       'picture': 'img/user-2.png'
@@ -129,7 +129,7 @@
     'review_usefulness': 24,
     'rating': 3,
     'description': 'Игра очень неплохая. Тут есть и трюки, и взлёты, и падения. Никогда не знаешь, что ждёт тебя впереди.'
-    }, {
+  }, {
     'author': {
       'name': 'Гейб Ньюэлл',
       'picture': 'img/dwjiqo9'
@@ -137,43 +137,40 @@
     'review_usefulness': 10,
     'rating': 5,
     'description': 'Игра очень интересная. Нравится возможность выбирать между героями, а самое крутое, что есть альтернативные концовки в игре. Она точно стоит своих денег.'
-    }];
+  }];
 
-    var reviewsFilter = document.querySelector('.reviews-filter');
-    var template = document.querySelector('template');
-    var reviewList = document.querySelector('.reviews-list');
-    var templateContainer = 'content' in template ? template.content : template;
-    reviewsFilter.classList.add('invisible');
-
-    var getReviewElement = function(reviews) {
-      var reviewElement = templateContainer.querySelector('.review').cloneNode(true);
-      var reviewAuthor = reviewElement.querySelector('.review-author'); 
-      var reviewRating = reviewElement.querySelector('.review-rating');
-      var reviewText = reviewElement.querySelector('.review-text');
-      var valueRatingClass = ['one', 'two', 'three', 'four', 'five'];
-
-      reviewAuthor.textContent = reviews.author.name;
-      reviewRating.classList.add('review-rating-' + valueRatingClass[reviews.rating - 1]);
-      reviewText.textContent = reviews.description;
-
-      var authorImage = new Image();
-      authorImage.onload = function() {
-        reviewAuthor.src = reviews.author.picture;
-        reviewAuthor.width = 124;
-        reviewAuthor.height = 124;
-      };
-
-      authorImage.onerror = function() {
-        reviewElement.classList.add('review-load-failure');
-      };
-
-      authorImage.src = reviews.author.picture;
-
-      return reviewElement;
+  var reviewsFilter = document.querySelector('.reviews-filter');
+  var template = document.querySelector('template');
+  var reviewList = document.querySelector('.reviews-list');
+  var valueRatingClass = ['one', 'two', 'three', 'four', 'five'];
+  var templateContainer = 'content' in template ? template.content : template;
+  var templateContainerReview = templateContainer.querySelector('.review');
+  reviewsFilter.classList.add('invisible');
+  var getReviewElement = function(reviewsArr) {
+    var reviewElement = templateContainerReview.cloneNode(true);
+    var reviewAuthor = reviewElement.querySelector('.review-author');
+    var reviewRating = reviewElement.querySelector('.review-rating');
+    var reviewText = reviewElement.querySelector('.review-text');
+    reviewRating.classList.add('review-rating-' + valueRatingClass[reviewsArr.rating - 1]);
+    reviewText.textContent = reviewsArr.description;
+    var authorImage = new Image();
+    authorImage.onload = function() {
+      reviewAuthor.alt = reviewsArr.author.name;
+      reviewAuthor.src = reviewsArr.author.picture;
+      reviewAuthor.width = 124;
+      reviewAuthor.height = 124;
     };
-  
-    reviews.forEach(function(review) {
-      reviewList.appendChild(getReviewElement(review));
-    });
-    reviewsFilter.classList.remove('invisible');
+
+    authorImage.onerror = function() {
+      reviewElement.classList.add('review-load-failure');
+    };
+
+    authorImage.src = reviewsArr.author.picture;
+
+    return reviewElement;
+  };
+  reviews.forEach(function(review) {
+    reviewList.appendChild(getReviewElement(review));
+  });
+  reviewsFilter.classList.remove('invisible');
 })();
