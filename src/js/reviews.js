c798543085cdc3bd0f73c0b04fdc847c47037b17
Nastya <nastyaskoblova@mail.ru>
2016-10-27 23:18:57 +0300

    fix stupid bug, reviews equal data

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -8,7 +8,10 @@
   var templateContainer = 'content' in template ? template.content : template;
   var templateContainerReview = templateContainer.querySelector('.review');
   var REVIEWS_LOAD_URL = 'http://localhost:1507/api/reviews';
+  var reviews = [];
+
   reviewsFilter.classList.add('invisible');
+
   var getReviewElement = function(reviewItem) {
     var reviewElement = templateContainerReview.cloneNode(true);
     var reviewAuthor = reviewElement.querySelector('.review-author');
@@ -17,27 +20,12 @@
     reviewRating.classList.add('review-rating-' + valueRatingClass[reviewItem.rating - 1]);
     reviewText.textContent = reviewItem.description;
     var authorImage = new Image();
-    var reviews = [];
     authorImage.onload = function() {
       reviewAuthor.alt = reviewItem.author.name;
       reviewAuthor.src = reviewItem.author.picture;
       reviewAuthor.width = 124;
       reviewAuthor.height = 124;
     };
-
-    var loadReviews = function(url, callback, callbackName) {
-      if (!callbackName) {
-        callbackName = 'cb' + Date.now();
-      }
-      window[callbackName] = function(data) {
-        callback(data);
-      };
-
-      var script = document.createElement('script');
-      script.src = url + '?callback=' + callbackName;
-      document.body.appendChild(script);
-    };
-
     authorImage.onerror = function() {
       reviewElement.classList.add('review-load-failure');
     };
@@ -47,11 +35,26 @@
     return reviewElement;
   };
 
+  var loadReviews = function(url, callback, callbackName) {
+    if (!callbackName) {
+      callbackName = 'cb' + Date.now();
+    }
+    window[callbackName] = function(data) {
+      reviews = data;
+      callback(data);
+    };
+
+    var script = document.createElement('script');
+    script.src = url + '?callback=' + callbackName;
+    document.body.appendChild(script);
+  };
+
   var showReviews = function(reviewItem) {
     reviews.forEach(function(review) {
       reviewList.appendChild(getReviewElement(review));
     });
   }
-  loadReviews(REVIEWS_LOAD_URL, showReviews, '__jsonpCallback');
+
+  loadReviews(REVIEWS_LOAD_URL, showReviews);
   reviewsFilter.classList.remove('invisible');
 })();
