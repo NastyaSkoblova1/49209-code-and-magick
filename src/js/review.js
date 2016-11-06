8f728571ecf7891ced869df869f06f3f8962bab7
Nastya <nastyaskoblova@mail.ru>
2016-11-06 11:47:48 +0300

    fix Review module and add review

--- src/js/review.js
+++ src/js/review.js
@@ -1,34 +1,34 @@
 'use strict';
 
-var Review = function(reviewItem, reviewElement) {
+var template = document.querySelector('template');
+var templateContainer = 'content' in template ? template.content : template;
+var templateContainerReview = templateContainer.querySelector('.review');
+var reviewElement = templateContainerReview.cloneNode(true);
+var valueRatingClass = ['one', 'two', 'three', 'four', 'five'];
+
+var Review = function(reviewItem) {
   this.data = reviewItem;
   this.element = reviewElement;
+  this.reviewAuthor = this.element.querySelector('.review-author');
+  this.reviewRating = this.element.querySelector('.review-rating');
+  this.reviewText = this.element.querySelector('.review-text');
+  this.reviewText.textContent = this.data.description;
+  this.authorImage = new Image();
+  this.authorImage.src = this.data.author.picture;
   this.reviewQuizAnswer = this.element.querySelectorAll('.review-quiz-answer');
 };
 
-Review.prototype.addReview = function(reviewItem) {
-  var template = document.querySelector('template');
-  var templateContainer = 'content' in template ? template.content : template;
-  var templateContainerReview = templateContainer.querySelector('.review');
-  var reviewElement = templateContainerReview.cloneNode(true);
-  var reviewAuthor = reviewElement.querySelector('.review-author');
-  var reviewRating = reviewElement.querySelector('.review-rating');
-  var reviewText = reviewElement.querySelector('.review-text');
-  var valueRatingClass = ['one', 'two', 'three', 'four', 'five'];
-  reviewRating.classList.add('review-rating-' + valueRatingClass[reviewItem.rating - 1]);
-  reviewText.textContent = reviewItem.description;
-  var authorImage = new Image();
-  authorImage.onload = function() {
-    reviewAuthor.alt = reviewItem.author.name;
-    reviewAuthor.src = reviewItem.author.picture;
-    reviewAuthor.width = 124;
-    reviewAuthor.height = 124;
+Review.prototype.addReview = function() {
+  this.reviewRating.classList.add('review-rating-' + valueRatingClass[this.element.rating - 1]);
+  this.authorImage.onload = function() {
+    this.reviewAuthor.alt = this.data.author.name;
+    this.reviewAuthor.src = this.data.author.picture;
+    this.reviewAuthor.width = 124;
+    this.reviewAuthor.height = 124;
   };
-  authorImage.onerror = function() {
-    reviewElement.classList.add('review-load-failure');
+  this.authorImage.onerror = function() {
+    this.element.classList.add('review-load-failure');
   };
-
-  authorImage.src = reviewItem.author.picture;
 };
 
 Review.prototype.setActive = function() {
