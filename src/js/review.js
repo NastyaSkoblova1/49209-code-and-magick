965fa754f8d8a986a325db0cb4e23caec188ce08
Nastya <nastyaskoblova@mail.ru>
2016-11-07 22:31:55 +0300

    fix element reviewElement and setActive function

--- src/js/review.js
+++ src/js/review.js
@@ -1,26 +1,26 @@
 'use strict';
 
-var template = document.querySelector('template');
-var templateContainer = 'content' in template ? template.content : template;
-var templateContainerReview = templateContainer.querySelector('.review');
-var reviewElement = templateContainerReview.cloneNode(true);
-var valueRatingClass = ['one', 'two', 'three', 'four', 'five'];
-
 var Review = function(reviewItem) {
+  var template = document.querySelector('template');
+  var templateContainer = 'content' in template ? template.content : template;
+  var templateContainerReview = templateContainer.querySelector('.review');
+  this.element = templateContainerReview.cloneNode(true);
+  this.valueRatingClass = ['one', 'two', 'three', 'four', 'five'];
   this.data = reviewItem;
-  this.reviewAuthor = reviewElement.querySelector('.review-author');
-  this.reviewRating = reviewElement.querySelector('.review-rating');
-  this.reviewText = reviewElement.querySelector('.review-text');
+  this.reviewAuthor = this.element.querySelector('.review-author');
+  this.reviewRating = this.element.querySelector('.review-rating');
+  this.reviewText = this.element.querySelector('.review-text');
   this.reviewText.textContent = this.data.description;
   this.authorImage = new Image();
+  this.reviewQuizAnswer = this.element.querySelectorAll('.review-quiz-answer');
+
   this.addReview();
-  this.element = reviewElement;
-  this.reviewQuizAnswer = reviewElement.querySelectorAll('.review-quiz-answer');
-  this.reviewRating.classList.add('review-rating-' + valueRatingClass[reviewElement.rating - 1]);
+  this.setActive();
 };
 
 Review.prototype.addReview = function() {
   var self = this;
+  this.reviewRating.classList.add('review-rating-' + this.valueRatingClass[this.data.rating - 1]);
   this.authorImage.onload = function() {
     self.reviewAuthor.alt = self.data.author.name;
     self.reviewAuthor.src = self.data.author.picture;
@@ -35,9 +35,14 @@ Review.prototype.addReview = function() {
 };
 
 Review.prototype.setActive = function() {
+  var self = this;
+
   for (var i = 0; i < this.reviewQuizAnswer.length; i++) {
     this.reviewQuizAnswer[i].onclick = function() {
-      this.reviewQuizAnswer[i].classList.add('review-quiz-answer-active');
+      for (i = 0; i < self.reviewQuizAnswer.length; i++) {
+        self.reviewQuizAnswer[i].classList.remove('review-quiz-answer-active');
+      }
+      this.classList.add('review-quiz-answer-active');
     };
   }
 };
